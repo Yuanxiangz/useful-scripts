02ee07e9e60d3033f77321b0aab9adccd71360f4
yuanxiangz <yuanxiangz@tujia.com>
2019-10-30 21:22:27 +0800

    change script

--- test-cases/self-installer.sh
+++ test-cases/self-installer.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if which svn &> /dev/null; then
-	[ ! -d "/tmp/useful-scripts-$USER" ] &&
-	svn checkout https://github.com/oldratlee/useful-scripts/trunk "/tmp/useful-scripts-$USER"
+if [ ! -d "/tmp/useful-scripts" ]; then
+	mkdir -p /tmp/useful-scripts
 fi
 
-export PATH="$PATH:/tmp/useful-scripts-$USER"
+wget -N -P /tmp/useful-scripts https://raw.githubusercontent.com/Yuanxiangz/useful-scripts/master/show-busy-java-threads
+chmod +x /tmp/useful-scripts/show-busy-java-threads
+wget -N -P /tmp/useful-scripts https://raw.githubusercontent.com/Yuanxiangz/useful-scripts/master/show-duplicate-java-classes
+chmod +x /tmp/useful-scripts/show-duplicate-java-classes
+wget -N -P /tmp/useful-scripts https://raw.githubusercontent.com/Yuanxiangz/useful-scripts/master/tcp-connection-state-counter
+chmod +x /tmp/useful-scripts/tcp-connection-state-counter
+wget -N -P /tmp/useful-scripts https://raw.githubusercontent.com/Yuanxiangz/useful-scripts/master/find-in-jars
+chmod +x /tmp/useful-scripts/find-in-jars
+
+export PATH="$PATH:/tmp/useful-scripts"
